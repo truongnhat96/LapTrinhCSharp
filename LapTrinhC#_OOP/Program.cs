c1a6ea93116ae7271c16204ed484bea680bb64cb
Luong Nhat Truong <luongnhattruong2004@gmail.com>
2024-06-02 19:37:20 +0700

    new update

---
+++ LapTrinhC#_OOP/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace truongdz
+{
+    class Nguoi
+    {
+        public void In(int x)
+        {
+            Console.WriteLine(x);
+        }
+    }
+    internal class Test
+    {
+       public static void Main()
+        {
+            string s = Console.ReadLine();
+            Console.WriteLine(s);
+            int x = Console.Read();
+            Console.WriteLine(x);
+            
+          }
+       }
+}
